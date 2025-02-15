3dc3fd104136f0706642ddfb8d5c8ed4a5ca0c9e
Neon <neonmed571@gmail.com>
2025-02-15 19:46:28 +0200

    investigating vercel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ const favBooks = [
   { id: 'id-1', name: 'JS for beginners' },
   { id: 'id-2', name: 'React basics' },
   { id: 'id-3', name: 'React Router overview' },
+  { id: 'id-4', name: 'TypeScript main' },
+  { id: 'id-5', name: 'All about Node.js' },
 ];
 
 const BookList = ({ books }) => {
